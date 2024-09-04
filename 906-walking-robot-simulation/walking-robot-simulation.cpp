99c0a48b0c1a10f4c6359d60db8437b4f1951fdf
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-09-04 16:49:39 +0530

    Time: 46 ms (98.90%) | Memory: 36.2 MB (99.45%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        const long long M = 6e4+1, lb = -3e4;
+        unordered_set<long long> ob;
+        ob.reserve(obstacles.size());
+
+        for(auto &i:obstacles){
+            long long x = i[0]-lb, y = i[1]-lb;
+            ob.insert(x*M+y);
+        }
+
+        const int dir[4][2]={{0,1},{-1,0},{0,-1},{1,0}};
+        int x=0, y=0, dx=0, dy=1, face=0, maxD2=0;
+
+        for(int i:commands){
+            switch(i){
+                case -2:
+                    face=(face+1)%4;
+                    dx=dir[face][0];
+                    dy=dir[face][1];
+                    break;
+                case -1: 
+                    face=(face+3)%4; 
+                    dx=dir[face][0]; 
+                    dy=dir[face][1]; 
+                    break;
+                default:
+                    for(int j=0; j<i; j++){
+                        x+=dx, y+=dy;
+                        if (ob.count((x-lb)*M+y-lb)) {
+                            x-=dx;
+                            y-=dy;
+                            break;
+                        }
+                        maxD2=max(maxD2, x*x+y*y);
+                    }
+            }
+        }
+        return maxD2;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
