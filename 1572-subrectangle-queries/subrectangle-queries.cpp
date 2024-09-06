44781aa6bf5cb4f178ab7a042b95e73e68f0f202
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-09-06 13:01:08 +0530

    Time: 33 ms (28.16%) | Memory: 23.7 MB (53.00%) - LeetSync

---
+++ 1572-subrectangle-queries/subrectangle-queries.cpp
@@ -0,0 +1,31 @@
+class SubrectangleQueries {
+public:
+    vector<vector<int>> rect;
+    int r,c;
+    SubrectangleQueries(vector<vector<int>>& rectangle) {
+        r=rectangle.size();
+        c=rectangle[0].size();
+        rect.resize(r, vector<int>(c));
+        for(int i=0;i<r;i++)
+            for(int j=0;j<c;j++)
+                rect[i][j]=rectangle[i][j];
+    }
+    
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for(int i=row1;i<=row2;i++)
+            for(int j=col1;j<=col2;j++)
+                rect[i][j]=newValue;
+    }
+    
+    int getValue(int row, int col) {
+        return rect[row][col];
+    }
+};
+
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries* obj = new SubrectangleQueries(rectangle);
+ * obj->updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj->getValue(row,col);
+ */
