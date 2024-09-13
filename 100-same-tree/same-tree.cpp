3f75425e736a8fcaecd7c3affbb8b7e0bc2f667b
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-09-13 14:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (36.48%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,20 +11,10 @@
  */
 class Solution {
 public:
-    bool ans;
-    void dfs(TreeNode* r1, TreeNode* r2){
-        if(!r1 && !r2) return;
-        if(!r1 || !r2 || r1->val!=r2->val){
-            ans=false;
-            return;
-        }
-        dfs(r1->left, r2->left);
-        dfs(r1->right, r2->right);
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        ans=true;
-        dfs(p,q);
-        return ans;
+    bool isSameTree(TreeNode* r1, TreeNode* r2) {
+        if(!r1 && !r2) return true;
+        if(!r1 || !r2 || r1->val!=r2->val) return false;
+        return isSameTree(r1->left, r2->left) && isSameTree(r1->right, r2->right);
     }
 };
 
