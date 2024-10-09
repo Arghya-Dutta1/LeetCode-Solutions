973ae5c15a35ee6b00ea6d6b182c4c4451a63698
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-10-09 14:35:45 +0530

    Time: 119 ms (98.76%) | Memory: 121 MB (52.74%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -32,3 +32,10 @@ public:
         return ranges::max(ans);
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
