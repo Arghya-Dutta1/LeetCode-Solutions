d7551d7573ec8ecd5b2c7bd24fafa9aa503786dd
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-10-15 15:48:13 +0530

    Time: 7 ms (98.98%) | Memory: 34.7 MB (31.98%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ps(n);
-        ps[0]=nums[0];
-        for(int i=1;i<n;i++)
-            ps[i]=ps[i-1]+nums[i];
-        for(int i=0;i<n;i++){
-            if(i==0){
-                if(ps[n-1]-ps[0]==0) return 0;
-            }
-            else if(i==n-1){
-                if(ps[n-2]==0) return n-1;
-            }
-            else{
-                if(ps[n-1]-ps[i] == ps[i-1]) return i;
-            }
+        int s=accumulate(nums.begin(), nums.end(), 0);
+        int p=0;
+        for(int i=0;i<nums.size();i++){
+            if(p==s-p-nums[i]) return i;
+            p+=nums[i];
         }
         return -1;
     }
