c504accc5b19be687a23edb1787aface7826d0cd
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-10-16 16:05:08 +0530

    Time: 3 ms (43.32%) | Memory: 8 MB (17.06%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
         string ans="";
-        priority_queue<pair<int, char>> pq;
-        if(a>0) pq.push({a, 'a'});
-        if(b>0) pq.push({b, 'b'});
-        if(c>0) pq.push({c, 'c'});
+        auto cmp = [](pair<char, int>& a, pair<char, int>& b){
+            return a.second<b.second;
+        };
+        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> pq(cmp);
+        if(a>0) pq.push({'a', a});
+        if(b>0) pq.push({'b', b});
+        if(c>0) pq.push({'c', c});
 
         while(!pq.empty()){
             auto it=pq.top();
             pq.pop();
-            int f=it.first;
-            char ch=it.second;
+            int f=it.second;
+            char ch=it.first;
             int n=ans.size();
             if(n>=2 && ans[n-1]==ch && ans[n-2]==ch){
                 if(pq.empty()) break;
                 auto nxt=pq.top();
                 pq.pop();
-                ans+=nxt.second;
-                if(--nxt.first>0) pq.push(nxt);
+                ans+=nxt.first;
+                if(--nxt.second>0) pq.push(nxt);
                 pq.push(it);
             }
             else{
                 ans+=ch;
-                if(--f>0) pq.push({f, ch});
+                if(--f>0) pq.push({ch, f});
             }
         }
         return ans;
