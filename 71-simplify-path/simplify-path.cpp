14a2054429976c2ec70bd9869919d98922578c71
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-10-23 15:47:46 +0530

    Time: 11 ms (6.23%) | Memory: 13.1 MB (7.35%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    vector<string> split(string str, char del){
+        string temp = "";
+        vector<string> ans;
+        for(int i=0; i<(int)str.size(); i++){
+            if(str[i] != del)
+                temp += str[i];
+            else{
+                if(temp!="" && temp!=" ")
+                    ans.push_back(temp);
+                temp="";
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+    }
+
+    string simplifyPath(string path) {
+        for(int i=0;i<path.size()-1;i++)
+            if(path[i]=='/' && path[i+1]=='/')
+                path.erase(i+1, 1);
+        int n=path.size();
+        if(path.size()>1 && path[n-1]=='/')
+            path.erase(n-1);
+        
+        if(path.size()==1) return path;
+
+        vector<string> s=split(path, '/');
+        stack<string> st;
+        for(auto i:s){
+            cout<<i<<endl;
+            if(i=="."){
+                continue;
+            }
+            else if(i==".."){
+                if(!st.empty())
+                    st.pop();
+            }
+            else
+                st.push(i);
+        }
+
+        if(st.empty()) return "/";
+
+        string ans="/"+st.top();
+        st.pop();
+        while(!st.empty()){
+            ans="/"+st.top()+ans;
+            st.pop();
+        }
+
+        for(int i=0;i<ans.size()-1;i++)
+            if(ans[i]=='/' && ans[i+1]=='/')
+                ans.erase(i+1, 1);
+
+        if(ans.size()>1 &&ans[ans.size()-1]=='/') return ans.substr(0, ans.size()-1);
+        // if(ans.size()==0) return "/";
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
