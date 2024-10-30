49fe18ea35907a898a0875fb1394f1e08db5ab96
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-10-30 09:57:07 +0530

    Time: 59 ms (56.06%) | Memory: 14.9 MB (67.26%) - LeetSync

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n=nums.size(), ans=0;
+        vector<int> left(n, 1), right(n, 1);
+
+        for(int i=1;i<n;i++)
+            for(int j=0;j<i;j++)
+                if(nums[i]>nums[j])
+                    left[i]=max(left[i], left[j]+1);
+        
+        for(int i=n-2;i>=0;i--)
+            for(int j=i+1;j<n;j++)
+                if(nums[i]>nums[j])
+                    right[i]=max(right[i], right[j]+1);
+        
+        for(int i=0;i<n;i++)
+            if(left[i]>1 && right[i]>1)
+                ans=max(ans, left[i]+right[i]-1);
+
+        return n-ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
