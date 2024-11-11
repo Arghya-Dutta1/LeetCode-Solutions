6b713aa03bb3242c8223bb429ed9d74b3d4a0d8c
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-11-11 10:47:20 +0530

    Time: 123 ms (6.22%) | Memory: 29.3 MB (20.13%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> prime;
+    void sieve(){
+        int n=1e3;
+        vector<bool> f(n+1, true);
+        for(int p=2;p*p<=n;p++)
+            if(f[p])
+                for(int i=p*p;i<=n;i+=p)
+                    f[i]=false;
+        for(int p=2;p<=n;p++)
+            if(f[p]) prime.push_back(p);
+    }
+    bool primeSubOperation(vector<int>& nums) {
+        sieve();
+        auto it=lower_bound(prime.begin(), prime.end(), nums[0]);
+        if(it!=prime.begin())
+            it--;
+        if(*it<nums[0])    
+            nums[0]-=*it;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]){
+                auto it=lower_bound(prime.begin(), prime.end(), nums[i]-nums[i-1]-1);
+                cout<<*it<<endl;
+                if(it!=prime.begin() && nums[i]-*it<=nums[i-1])
+                    it--;
+                cout<<*it<<endl;
+                if(nums[i]-*it>nums[i-1])
+                    nums[i]-=*it;
+            }
+        }
+
+        for(auto i:nums) cout<<i<<" ";
+
+        for(int i=1;i<nums.size();i++)
+            if(nums[i]<=nums[i-1]) return false;
+        return true;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
