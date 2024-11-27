414a12c56ceab7ffbc9f20d628debab4c5e82b98
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-11-27 14:14:23 +0530

    Time: 379 ms (31.19%) | Memory: 128.5 MB (50.08%) - LeetSync

---
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<pair<int, int>>> graph(n);
+        for (int i = 0; i < n - 1; ++i)
+            graph[i].emplace_back(i + 1, 1); 
+
+        vector<int> result;
+        
+        for (const auto& query : queries) {
+            int u = query[0];
+            int v = query[1];
+            graph[u].emplace_back(v, 1); 
+            result.push_back(dijkstra(graph, n));
+        }
+        
+        return result;
+    }
+
+    int dijkstra(const vector<vector<pair<int, int>>>& graph, int n) {
+        vector<int> dist(n, INT_MAX);
+        dist[0] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.emplace(0, 0);
+        
+        while (!pq.empty()) {
+            int d = pq.top().first;
+            int u = pq.top().second;
+            pq.pop();
+            
+            if (d > dist[u]) continue;
+            
+            for (const auto& [v, w] : graph[u]) {
+                if (dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.emplace(dist[v], v);
+                }
+            }
+        }
+        
+        return dist[n - 1];
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
