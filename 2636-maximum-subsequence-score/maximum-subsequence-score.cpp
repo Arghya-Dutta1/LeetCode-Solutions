3ef3ed08941f0eb67ad4f57cbca1423bfc4febeb
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-11-28 17:37:43 +0530

    Time: 39 ms (99.02%) | Memory: 95.7 MB (67.52%) - LeetSync

--- 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
         int n=nums1.size();
+
+        auto func=[&](auto &v1, auto &v2) {
+            return v1.second>v2.second;
+        };
+
         vector<pair<int, int>> v;
         for(int i=0;i<n;i++)
-            v.push_back({nums2[i], nums1[i]});
-        sort(v.begin(), v.end(), greater<>());
+            v.push_back({nums1[i], nums2[i]});
+        sort(v.begin(), v.end(), func);
 
         priority_queue<int, vector<int>, greater<int>> pq;
         long long ksum=0;
         for(int i=0;i<k;i++){
-            ksum+=v[i].second;
-            pq.push(v[i].second);
+            ksum+=v[i].first;
+            pq.push(v[i].first);
         }
-        long long ans=ksum*v[k-1].first;
+        long long ans=ksum*v[k-1].second;
         
         for(int i=k;i<n;i++){
-            ksum+=v[i].second-pq.top();
+            ksum+=v[i].first-pq.top();
             pq.pop();
-            pq.push(v[i].second);
+            pq.push(v[i].first);
 
-            ans=max(ans, ksum*v[i].first);
+            ans=max(ans, ksum*v[i].second);
         }
 
         return ans;
