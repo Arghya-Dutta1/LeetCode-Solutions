88b257828745b45b021b001638fff4c2f3feba31
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-12-08 11:51:50 +0530

    Time: 8 ms (29.32%) | Memory: 24 MB (24.69%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        int l=nums[0][0],r=nums[0][1];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i][0]<=r){
+                r=max(r, nums[i][1]);
+            }
+            else{
+                ans.push_back({l,r});
+                l=nums[i][0];
+                r=nums[i][1];
+            }
+        }
+        ans.push_back({l,r});
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
