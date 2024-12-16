b8ee36a923aae83551f38571b4f4bbb78822f637
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-12-16 13:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (10.47%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int count(vector<vector<int>>& v, int i, int j, int m, int n){
-        if(i>=m || j>=n || v[i][j]==1) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=count(v, i+1, j, m, n)+count(v, i, j+1, m, n);
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size(), n=obstacleGrid[0].size();
-        dp.resize(m, vector<int>(n, -1));
-        return count(obstacleGrid, 0, 0, m, n);
+        dp.resize(m+1, vector<int>(n+1, 0));
+        dp[0][1]=1;
+        // dp[1][0]=1;
+
+        for(int i=1;i<=m;i++)
+            for(int j=1;j<=n;j++)
+                if(!obstacleGrid[i-1][j-1])
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        return dp[m][n];
     }
 };
 
