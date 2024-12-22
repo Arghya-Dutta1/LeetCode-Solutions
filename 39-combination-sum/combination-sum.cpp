a4af7ffa67c2d21bcc379be12693d6055a65bd28
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-12-22 15:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (27.68%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> t;
+        function<void(int, int)> dfs = [&](int i, int s) {
+            if (s == 0) {
+                ans.emplace_back(t);
+                return;
+            }
+            if (i >= candidates.size() || s < candidates[i]) {
+                return;
+            }
+            dfs(i + 1, s);
+            t.push_back(candidates[i]);
+            dfs(i, s - candidates[i]);
+            t.pop_back();
+        };
+        dfs(0, target);
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
