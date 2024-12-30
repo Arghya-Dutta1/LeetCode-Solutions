495ac8e7e6c316093cf5b29163c09e9f28d0e096
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2024-12-30 09:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (17.93%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        ranges::sort(nums);
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
