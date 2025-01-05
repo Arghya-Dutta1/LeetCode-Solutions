3da96ca2d80959dec4488e501497bd67c77b1434
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-01-05 09:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (18.90%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        ranges::reverse(s);
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
