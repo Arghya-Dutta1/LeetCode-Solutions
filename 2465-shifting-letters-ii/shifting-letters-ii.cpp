909531efcb67654fa40f08ab9b8c60508de2a039
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-01-05 08:13:46 +0530

    Time: 8 ms (55.36%) | Memory: 97.7 MB (78.95%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int shift=0;
+        vector<int> t(s.size() + 1);
+
+        for(auto& sh:shifts){
+            int st=sh[0], e=sh[1], dir=sh[2];
+            int d=dir?1:-1;
+            t[st]+=d;
+            t[e+1]-=d;
+        }
+
+        for(int i=0; i<s.size(); i++){
+            shift=(shift+t[i])%26;
+            s[i]='a'+(s[i] -'a'+shift+26)%26;
+        }
+
+        return s;
+    }
+};
