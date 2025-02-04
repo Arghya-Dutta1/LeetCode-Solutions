cbee2d13cb4e4bcca51e8096bccac3ca6d72d688
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-02-04 23:03:58 +0530

    Time: 1 ms (51.17%) | Memory: 14.5 MB (60.05%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int> time(1001, 0);
+        for(auto& t:trips){
+            time[t[1]]+=t[0];
+            time[t[2]]-=t[0];
+        }
+
+        int cap=0;
+        for(int i:time){
+            cap+=i;
+            if(cap>capacity)
+                return false;
+        }
+
+        return true; 
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
