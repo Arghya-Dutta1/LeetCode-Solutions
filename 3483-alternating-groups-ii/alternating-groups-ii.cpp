9915a5bb06e28df234fb64dfed1b34b96469341c
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-03-09 09:50:36 +0530

    Time: 37 ms (100.00%) | Memory: 102.3 MB (100.00%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n=colors.size(), ans=0, cnt=0;
+        for(int i=0; i<n<<1; i++){
+            if(i && colors[i%n]==colors[(i-1)%n])
+                cnt=1;
+            else
+                cnt++;
+            ans+=i>=n&&cnt>=k?1:0;
+        }
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
