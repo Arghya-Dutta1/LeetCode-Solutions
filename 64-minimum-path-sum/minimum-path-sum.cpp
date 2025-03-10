0f592f4453aa933a9db15421072dcf265de25cd2
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-03-10 15:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (37.77%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    vector<vector<long long>> dp;
+    vector<vector<int>> dp;
     int solve(vector<vector<int>>& grid, int i, int j){
         if(i>=grid.size() || j>=grid[0].size())
             return INT_MAX;
         if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=(long long)grid[i][j]+min(solve(grid, i, j+1), solve(grid, i+1, j));
+        return dp[i][j]=grid[i][j]+min(solve(grid, i, j+1), solve(grid, i+1, j));
     }
 
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(), n=grid[0].size();
-        dp.resize(m, vector<long long>(n, -1));
+        dp.resize(m, vector<int>(n, -1));
         return solve(grid, 0, 0);
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
