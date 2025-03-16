3bf626f3a026e19642e233ab85a902d90ec6498e
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-03-16 10:16:31 +0530

    Time: 8 ms (50.00%) | Memory: 46.9 MB (91.67%) - LeetSync

--- 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
+++ 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
@@ -3,13 +3,16 @@ public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size(), m = queries.size();
         bool z = true;
-        for (auto x : nums) {
-            if(x != 0) { z = false; break; }
-        }
-        if(z) return 0;
+        for (auto x : nums)
+            if (x != 0) {
+                z = false;
+                break;
+            }
+        if (z)
+            return 0;
 
         int lo = 1, hi = m, res = -1;
-        while(lo <= hi) {
+        while (lo <= hi) {
             int mid = (lo + hi) / 2;
             bool poss = true;
             for (int j = 0; j < n; j++) {
@@ -18,19 +21,26 @@ public:
                 dp.reset();
                 dp[0] = 1;
                 for (int i = 0; i < mid; i++) {
-                    if(j < queries[i][0] || j > queries[i][1]) continue;
+                    if (j < queries[i][0] || j > queries[i][1])
+                        continue;
                     dp |= (dp << queries[i][2]);
                 }
-                if(!dp[tar]) { poss = false; break; }
+                if (!dp[tar]) {
+                    poss = false;
+                    break;
+                }
             }
-            if(poss) { res = mid; hi = mid - 1; }
-            else lo = mid + 1;
+            if (poss) {
+                res = mid;
+                hi = mid - 1;
+            } else
+                lo = mid + 1;
         }
         return res;
     }
 };
 
-static const int __ = [](){
+static const int __ = []() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
