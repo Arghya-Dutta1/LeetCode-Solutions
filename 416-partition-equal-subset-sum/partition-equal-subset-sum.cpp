992139f1a771ee383e9095fb0e3a180f78e498cf
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-03-18 19:05:54 +0530

    Time: 79 ms (78.68%) | Memory: 30.7 MB (53.06%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& A) {
+        int total = accumulate(begin(A), end(A), 0);
+        if (total % 2) return false;
+        unordered_set<int> s, next;
+        for (int n : A) {
+            next = s;
+            for (int m : s) next.insert(m + n);
+            next.insert(n);
+            if (next.count(total / 2)) return true;
+            swap(s, next);
+        }
+        return false;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
