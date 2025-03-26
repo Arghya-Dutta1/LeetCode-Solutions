aa642dcb32ce7d0d0af7ac18cf69857fa3dbe8dd
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-03-26 11:09:00 +0530

    Time: 715 ms (5.47%) | Memory: 90.3 MB (20.97%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> g;
+        for(auto& i:grid) g.insert(g.begin(), i.begin(), i.end());
+        ranges::sort(g);
+        int r=g[0]%x;
+        for(auto i:g)
+            if(i%x!=r) return -1;
+        int n=g.size(), ans=0, m;
+        // if(n%2==0) m=(g[n/2-1]+g[n/2])/2;
+        // else 
+        m=g[n/2];
+        for(auto i:g) ans+=abs(m-i)/x;
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
