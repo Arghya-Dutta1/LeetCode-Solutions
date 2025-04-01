7789e6aede4b1f92a47e556bacfdb8dd7efdb0d8
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-01 15:09:23 +0530

    Time: 11 ms (32.59%) | Memory: 126.9 MB (42.28%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     vector<long long> dp;
-    
-    long long solve(vector<vector<int>>& q, int idx) {
-        if (idx >= q.size()) return 0;
-        if (dp[idx] != -1) return dp[idx];
-
-        // Take the current question
-        long long take = q[idx][0] + solve(q, idx + q[idx][1] + 1);
-        
-        // Skip the current question
-        long long notTake = solve(q, idx + 1);
-
-        return dp[idx] = max(take, notTake);
+    long long solve(vector<vector<int>>& q, int idx){
+        if(idx>=q.size()) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        long long t=q[idx][0]+solve(q, idx+q[idx][1]+1);
+        long long nt=solve(q, idx+1);
+        return dp[idx]=max(t,nt);
     }
 
     long long mostPoints(vector<vector<int>>& questions) {
@@ -20,3 +14,10 @@ public:
         return solve(questions, 0);
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
