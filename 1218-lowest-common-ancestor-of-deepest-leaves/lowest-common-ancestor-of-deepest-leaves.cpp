8eca0aa6387b86200824568bd9f4712c5970b6f9
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-04 11:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (11.96%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root==p || root==q)
+            return root;
+
+        TreeNode* left=lowestCommonAncestor(root->left, p, q);
+        TreeNode* right=lowestCommonAncestor(root->right, p, q); 
+
+        if(!left)
+            return right;
+        else if(!right)
+            return left;
+        else
+            return root;  
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        vector<vector<TreeNode*>> ans;
+        if(!root) return NULL;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int s=q.size();
+            vector<TreeNode*> lvl;
+            for(int i=0;i<s;i++){
+                TreeNode* curr=q.front();
+                q.pop();
+                lvl.push_back(curr);
+                if(curr->left)
+                    q.push(curr->left);
+                if(curr->right)
+                    q.push(curr->right);
+            }
+            ans.push_back(lvl);
+        }
+        vector<TreeNode*> b=ans.back();
+        if(b.size()==1) return b[0];
+        return lowestCommonAncestor(root, b[0], b.back());
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
