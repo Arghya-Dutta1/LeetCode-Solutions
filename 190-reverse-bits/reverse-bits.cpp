c8ad394263b611fe44523b44c029476f60232e53
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-08 11:31:52 +0530

    Time: 3 ms (33.30%) | Memory: 7.7 MB (87.05%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        for(int i=0;i<32;i++)
+            if(n>>i&1)
+                ans|=1<<31-i;
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
