e8bff7da601feab209a6817c6373b05c6e9d5c2c
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-25 16:37:41 +0530

    Time: 65 ms (34.43%) | Memory: 126.1 MB (24.59%) - LeetSync

---
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        int n = nums.size();
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i)
+            arr[i] = int(nums[i] % modulo == k);
+        
+        unordered_map<int, int> cnt;
+        cnt[0] = 1;
+        long long ans = 0;
+        int s = 0;
+        for (int x : arr) {
+            s += x;
+            ans += cnt[(s - k + modulo) % modulo];
+            cnt[s % modulo]++;
+        }
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
