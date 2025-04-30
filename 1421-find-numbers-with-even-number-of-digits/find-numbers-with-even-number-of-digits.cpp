ce675e5c80907de9133cc5c2d42cea146ff8646d
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-04-30 14:13:28 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (11.46%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int ans=0;
+        for(auto i:nums){
+            string s=to_string(i);
+            if(s.size()%2==0) ans++;
+        }
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
