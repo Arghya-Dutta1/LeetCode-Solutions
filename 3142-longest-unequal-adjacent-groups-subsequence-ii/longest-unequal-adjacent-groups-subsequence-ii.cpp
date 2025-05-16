86c3a24354a73638a06bc044082bdfb5c1b99238
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-05-16 09:57:16 +0530

    Time: 84 ms (39.84%) | Memory: 34.5 MB (59.38%) - LeetSync

---
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n = words.size();
+        vector<int> dp(n, 1);
+        vector<int> parent(n, -1);
+        int maxLen = 1, lastIdx = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (groups[i] != groups[j] &&
+                    words[i].size() == words[j].size() &&
+                    hammingDistance(words[i], words[j]) == 1) {
+                    
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        parent[i] = j;
+                    }
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                lastIdx = i;
+            }
+        }
+        vector<string> result;
+        while (lastIdx != -1) {
+            result.push_back(words[lastIdx]);
+            lastIdx = parent[lastIdx];
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+
+private:
+    int hammingDistance(const string& a, const string& b) {
+        int dist = 0;
+        for (int i = 0; i < a.size(); ++i)
+            if (a[i] != b[i])
+                ++dist;
+        return dist;
+    }
+};
