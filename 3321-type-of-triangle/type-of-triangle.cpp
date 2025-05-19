9a41aad72c30bb91e68171525d7356c990e8756e
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-05-19 08:59:02 +0530

    Time: 3 ms (4.55%) | Memory: 23.3 MB (8.96%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        if(nums[0]>=nums[1]+nums[2] || nums[1]>=nums[0]+nums[2] || nums[2]>=nums[1]+nums[0]) return "none";
+        switch(st.size()){
+            case 1: return "equilateral";
+            case 2: return "isosceles";
+            case 3: return "scalene";
+        }
+        return "none";
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
