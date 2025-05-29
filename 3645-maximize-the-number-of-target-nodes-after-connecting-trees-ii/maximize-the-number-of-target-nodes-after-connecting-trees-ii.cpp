5a7f486170afeffa720cdc05f45a3313c5a7ef9a
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-05-29 11:57:11 +0530

    Time: 272 ms (97.28%) | Memory: 342.1 MB (54.42%) - LeetSync

---
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        auto g1 = build(edges1);
+        auto g2 = build(edges2);
+        int n = g1.size(), m = g2.size();
+        vector<int> c1(n, 0), c2(m, 0);
+        vector<int> cnt1(2, 0), cnt2(2, 0);
+
+        dfs(g2, 0, -1, c2, 0, cnt2);
+        dfs(g1, 0, -1, c1, 0, cnt1);
+
+        int t = max(cnt2[0], cnt2[1]);
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i)
+            ans[i] = t + cnt1[c1[i]];
+        return ans;
+    }
+
+private:
+    vector<vector<int>> build(const vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+        vector<vector<int>> g(n);
+        for (auto& e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        return g;
+    }
+
+    void dfs(const vector<vector<int>>& g, int a, int fa, vector<int>& c, int d, vector<int>& cnt) {
+        c[a] = d;
+        cnt[d]++;
+        for (auto b : g[a])
+            if (b != fa)
+                dfs(g, b, a, c, d ^ 1, cnt);
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
