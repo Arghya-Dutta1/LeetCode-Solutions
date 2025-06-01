1f1b2b14e2303d9962905f2c13ece40090bbe774
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-06-01 17:50:49 +0530

    Time: 23 ms (62.94%) | Memory: 8.9 MB (84.71%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     long long distributeCandies(int n, int limit) {
         long long ans=0;
-        for(int i=0;i<=min(n,limit);i++){
+        for(int i=0;i<=min(n,limit);i++)
             ans+=(long long)(max(min(limit, n - i) - max(0, n - i - limit) + 1, 0));
-        }
         return ans;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
