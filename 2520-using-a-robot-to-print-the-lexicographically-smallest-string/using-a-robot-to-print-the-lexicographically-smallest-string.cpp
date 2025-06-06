72f34b7685bae8a6e908a56509cfbcecae998157
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-06-06 20:40:39 +0530

    Time: 47 ms (68.81%) | Memory: 35 MB (30.20%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        int cnt[26] = {0};
+        for (char& c : s) ++cnt[c - 'a'];
+        char mi = 'a';
+        string stk;
+        string ans;
+        for (char& c : s) {
+            --cnt[c - 'a'];
+            while (mi < 'z' && cnt[mi - 'a'] == 0) ++mi;
+            stk += c;
+            while (!stk.empty() && stk.back() <= mi) {
+                ans += stk.back();
+                stk.pop_back();
+            }
+        }
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
