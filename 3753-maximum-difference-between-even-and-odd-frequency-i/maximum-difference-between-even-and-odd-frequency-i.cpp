71390991f9ef541ed9013d08c8d39758df530426
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-06-10 20:38:42 +0530

    Time: 3 ms (34.81%) | Memory: 10.1 MB (16.70%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -3,16 +3,12 @@ public:
     int maxDifference(string s) {
         unordered_map<char, int> mp;
         for(auto i:s) mp[i]++;
-        int em=INT_MIN, emn=INT_MAX, om=INT_MIN, omn=INT_MAX;
+        int emn=INT_MAX, om=INT_MIN;
         for(auto& [i,j]:mp){
-            if(j%2==0){
-                em=max(em,j);
+            if(j%2==0)
                 emn=min(emn,j);
-            }
-            else{
+            else
                 om=max(om,j);
-                omn=min(omn,j);
-            }
         }       
         return om-emn;            
     }
