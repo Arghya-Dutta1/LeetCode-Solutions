4afa741e10de4da34458a69d5b55aecb1b640ce4
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-06-13 19:16:26 +0530

    Time: 71 ms (5.18%) | Memory: 61.5 MB (5.05%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         int mn=INT_MAX;
         ranges::sort(arr);
-        map<int, vector<vector<int>>> mp;
+        unordered_map<int, vector<vector<int>>> mp;
         for(int i=1;i<arr.size();i++){
             int dif=arr[i]-arr[i-1];
             mp[dif].push_back({arr[i-1], arr[i]});
