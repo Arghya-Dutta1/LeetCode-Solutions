1dfb4ba9fa9e828106096a24efdb828cc6c000f3
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-06-29 20:13:16 +0530

    Time: 37 ms (28.84%) | Memory: 54.4 MB (34.63%) - LeetSync

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        ranges::sort(nums);
+        int mod = 1e9 + 7, n = nums.size();
+        vector<int> fastExp(n + 1);
+        fastExp[0] = 1;
+        for (int i = 1; i <= n; ++i)
+            fastExp[i] = (fastExp[i - 1] * 2) % mod;
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] * 2L > target)
+                break;
+            int j = upper_bound(nums.begin() + i + 1, nums.end(), target - nums[i]) - nums.begin() - 1;
+            count = (count + fastExp[j - i]) % mod;
+        }
+        return count;
+    }
+};
