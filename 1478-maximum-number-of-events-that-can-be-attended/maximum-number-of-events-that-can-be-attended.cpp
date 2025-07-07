f2fbcb8ac209e51dd8f95df1b5c71d33c020ddeb
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-07-07 19:15:39 +0530

    Time: 231 ms (10.17%) | Memory: 147.5 MB (7.36%) - LeetSync

---
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        int mnd=INT_MAX, mxd=0, ans=0;
+        unordered_map<int, vector<int>> mp;
+        for(auto i:events){
+            mp[i[0]].push_back(i[1]);
+            mnd=min(mnd, i[0]);
+            mxd=max(mxd, i[1]);
+        }
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=mnd;i<=mxd;i++){
+            while(!pq.empty() && pq.top()<i) pq.pop();
+            for(auto j:mp[i]) pq.push(j);
+            if(!pq.empty()){
+                ans++;
+                pq.pop();
+            }
+        }
+        return ans;
+    }
+};
