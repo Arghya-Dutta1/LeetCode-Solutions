dd34752ea91ecc91bc9c51909bd30128c4cd932b
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-07-17 20:04:12 +0530

    Time: 126 ms (33.63%) | Memory: 130.9 MB (21.83%) - LeetSync

---
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        vector<vector<int>> remainderCount(k, vector<int>(k, 0));
+        int maxLength = 0;
+        for (int num : nums) {
+            int modValue = num % k;
+            for (int remainder = 0; remainder < k; ++remainder) {
+                int targetRemainder = (remainder - modValue + k) % k;
+                remainderCount[modValue][targetRemainder] =
+                    remainderCount[targetRemainder][modValue] + 1;
+                maxLength =
+                    max(maxLength, remainderCount[modValue][targetRemainder]);
+            }
+        }
+        return maxLength;
+    }
+};
