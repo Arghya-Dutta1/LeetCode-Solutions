fb12e4414ca87cd973abdd00875326854981d5e5
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-07-23 19:34:47 +0530

    Time: 32 ms (73.45%) | Memory: 25.7 MB (71.09%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        if (x < y) {
+            reverse(s.begin(), s.end());
+            return maximumGain(s, y, x);
+        }
+        int ans = 0;
+        stack<char> stk1;
+        stack<char> stk2;
+        for (char c : s) {
+            if (c != 'b')
+                stk1.push(c);
+            else {
+                if (!stk1.empty() && stk1.top() == 'a') {
+                    stk1.pop();
+                    ans += x;
+                } else
+                    stk1.push(c);
+            }
+        }
+        while (!stk1.empty()) {
+            char c = stk1.top();
+            stk1.pop();
+            if (c != 'b')
+                stk2.push(c);
+            else {
+                if (!stk2.empty() && stk2.top() == 'a') {
+                    stk2.pop();
+                    ans += y;
+                } else
+                    stk2.push(c);
+            }
+        }
+        return ans;
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
