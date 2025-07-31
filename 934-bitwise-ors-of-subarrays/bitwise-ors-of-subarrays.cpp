60663973b70131ab8878515e7c30e166f0354dff
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-07-31 19:33:02 +0530

    Time: 312 ms (89.38%) | Memory: 135.9 MB (86.64%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -14,3 +14,10 @@ public:
         return unordered_set<int>(s.begin(), s.end()).size();
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
