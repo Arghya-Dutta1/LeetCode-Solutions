306e5aaab48e5d2b6f5a7fc37421b969d5543355
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-08-29 19:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (48.89%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -8,3 +8,10 @@ public:
         return static_cast<long>(xEven) * yOdd + static_cast<long>(yEven) * xOdd;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
