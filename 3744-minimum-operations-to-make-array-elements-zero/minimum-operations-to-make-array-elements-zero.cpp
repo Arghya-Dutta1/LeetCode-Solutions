e96d00048f96197396191a75b074f17fbfe33d07
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-09-06 20:15:21 +0530

    Time: 12 ms (79.71%) | Memory: 183.2 MB (94.20%) - LeetSync

---
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long minOperations(vector<vector<int>>& queries) {
+        long long ans = 0;
+        for (const vector<int>& query : queries) {
+            int l = query[0], r = query[1];
+            ans += (getOperations(r) - getOperations(l - 1) + 1) / 2;
+        }
+        return ans;
+    }
+    long long getOperations(int n) {
+        long long res = 0, ops = 0;
+        for (int powerOfFour = 1; powerOfFour <= n; powerOfFour *= 4) {
+            int l = powerOfFour, r = min(n, powerOfFour * 4 - 1);
+            res += static_cast<long long>(r - l + 1) * ++ops;
+        }
+        return res -1;
+    }
+};
