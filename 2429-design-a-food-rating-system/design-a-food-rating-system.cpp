902b8543515fb1f984b6bcba7059dfbc4f214452
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-09-17 19:38:52 +0530

    Time: 156 ms (42.38%) | Memory: 175 MB (13.85%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -27,6 +27,13 @@ public:
     }
 };
 
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
 /**
  * Your FoodRatings object will be instantiated and called as such:
  * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
