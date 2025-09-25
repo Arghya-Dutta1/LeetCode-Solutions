c4dd0b67a71d5c80a3b8464e6d1747e4d3626c24
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-09-25 19:09:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (99.70%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -8,3 +8,10 @@ public:
         return triangle[0][0];
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
