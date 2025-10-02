720608d219907769d684df1c6b4cc7ce8a77deb8
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-10-02 09:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (13.66%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.cpp
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
         int ans = numBottles;
-
         while (numBottles >= numExchange) {
             numBottles = (numBottles - numExchange + 1);
             ++numExchange;
             ++ans;
         }
-
         return ans;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
