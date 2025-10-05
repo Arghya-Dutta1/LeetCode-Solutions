f209f251f415313579fdfc570d3e63415f02edf5
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-10-05 08:22:35 +0530

    Time: 12 ms (36.15%) | Memory: 22.1 MB (95.12%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void dfs(const vector<vector<int>>& heights, int i, int j, vector<vector<bool>>& seen, int h = 0) {
+        if (i < 0 || i == heights.size() || j < 0 || j == heights[0].size())
+            return;
+        if (seen[i][j] || heights[i][j] < h)
+            return;
+
+        seen[i][j] = true;
+        dfs(heights, i + 1, j, seen, heights[i][j]);
+        dfs(heights, i - 1, j, seen, heights[i][j]);
+        dfs(heights, i, j + 1, seen, heights[i][j]);
+        dfs(heights, i, j - 1, seen, heights[i][j]);
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m = heights.size(), n = heights[0].size();
+        vector<vector<int>> ans;
+        vector<vector<bool>> seenP(m, vector<bool>(n));
+        vector<vector<bool>> seenA(m, vector<bool>(n));
+
+        for (int i = 0; i < m; ++i) {
+            dfs(heights, i, 0, seenP);
+            dfs(heights, i, n - 1, seenA);
+        }
+
+        for (int j = 0; j < n; ++j) {
+            dfs(heights, 0, j, seenP);
+            dfs(heights, m - 1, j, seenA);
+        }
+
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (seenP[i][j] && seenA[i][j])
+                    ans.push_back({i, j});
+
+        return ans;
+    }
+};
