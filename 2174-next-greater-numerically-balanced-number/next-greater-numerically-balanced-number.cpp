514035e5766d9d955f6f4440c96129d33e906d63
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-10-24 18:09:04 +0530

    Time: 537 ms (29.15%) | Memory: 254.9 MB (16.08%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isBalance(int num) {
+        vector<int> count(10);
+        while (num > 0) {
+            if (num % 10 == 0)
+                return false;
+            ++count[num % 10];
+            num /= 10;
+        }
+        for (int i = 1; i < 10; ++i)
+            if (count[i] && count[i] != i)
+                return false;
+        return true;
+    }
+    int nextBeautifulNumber(int n) {
+        while (!isBalance(++n));
+        return n;
+    }
+};
