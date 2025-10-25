666bd584cf16a372b8d170bde2f6d31eed795f06
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-10-25 16:26:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.24%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -13,3 +13,10 @@ public:
         return totalFromCompleteWeeks + totalFromRemainingDays;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
