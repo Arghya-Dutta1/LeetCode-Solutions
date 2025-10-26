40b2e0d606a7c01be6bd840e040cc12cb4387c92
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-10-26 11:35:07 +0530

    Time: 15 ms (43.63%) | Memory: 120.4 MB (100.00%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.cpp
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -32,6 +32,13 @@ private:
     }
 };
 
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
 /**
  * Your Bank object will be instantiated and called as such:
  * Bank* obj = new Bank(balance);
