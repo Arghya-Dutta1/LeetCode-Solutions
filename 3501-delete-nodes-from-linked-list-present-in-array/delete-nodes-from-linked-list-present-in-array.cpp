28168e0e45d319c83cec205bcb06e5a41579866f
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-11-01 13:42:29 +0530

    Time: 105 ms (41.15%) | Memory: 263.5 MB (41.15%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -11,23 +11,20 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> st(nums.begin(), nums.end());
-        ListNode* dummy=new ListNode(0);
-        dummy->next=head;
-        ListNode* curr=dummy;
-        while(curr->next){
-            if(st.find(curr->next->val)!=st.end()){
-                ListNode* temp=curr->next;
-                curr->next=curr->next->next;
-                delete temp;
-            }
-            else curr=curr->next;
-        }
-        return dummy->next;
+        ListNode dummy(0, head);
+        unordered_set<int> numsSet{nums.begin(), nums.end()};
+
+        for (ListNode* curr = &dummy; curr->next != nullptr;)
+            if (numsSet.contains(curr->next->val))
+                curr->next = curr->next->next;
+            else
+                curr = curr->next;
+
+        return dummy.next;
     }
 };
 
-static const int __ = [](){
+static const int __ = []() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
