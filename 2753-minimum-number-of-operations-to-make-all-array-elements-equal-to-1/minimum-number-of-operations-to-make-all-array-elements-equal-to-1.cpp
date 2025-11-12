2b335091609a36d4377ec5d62fd96192d364d6b7
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-11-12 19:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 31.7 MB (34.03%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -18,3 +18,10 @@ public:
         return minOps == INT_MAX ? -1 : minOps + n - 1;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
