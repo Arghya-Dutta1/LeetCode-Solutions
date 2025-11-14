86eb3823d7cedab5aaa7e75e44c4b60b9703ebda
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-11-14 23:47:07 +0530

    Time: 39 ms (52.04%) | Memory: 93.5 MB (15.67%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> ans(n, vector<int>(n)), prefix(n, vector<int>(n + 1));
+        for (const vector<int>& query : queries) {
+            int row1 = query[0], col1 = query[1], row2 = query[2], col2 = query[3];
+            for (int i = row1; i <= row2; ++i) {
+                ++prefix[i][col1];
+                --prefix[i][col2 + 1];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int sum = 0;
+            for (int j = 0; j < n; ++j) {
+                sum += prefix[i][j];
+                ans[i][j] = sum;
+            }
+        }
+        return ans;
+    }
+};
