945d4162b5050dd9bdae77a62d7c0939061a6d6d
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-11-23 10:03:46 +0530

    Time: 39 ms (38.95%) | Memory: 37 MB (43.08%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -5,7 +5,13 @@ public:
         for (auto num : nums)
             for (auto sum : vector<int>(dp))
                 dp[(sum + num) % 3] = max(dp[(sum + num) % 3], sum + num);
-
         return dp[0];
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
