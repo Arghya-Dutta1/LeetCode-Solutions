e3d3648181ce8d27eb14ee2a4110045f03605735
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-11-26 17:39:06 +0530

    Time: 259 ms (61.14%) | Memory: 138.4 MB (57.11%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -22,3 +22,10 @@ public:
         return numberOfPaths(grid, 0, 0, 0, k, mem);
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
