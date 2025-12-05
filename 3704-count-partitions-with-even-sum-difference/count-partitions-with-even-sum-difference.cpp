3bee6d96894b8aedcd1cc3880345ac390813762c
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2025-12-05 18:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (17.99%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -4,3 +4,10 @@ public:
         return accumulate(nums.begin(), nums.end(), 0) % 2 == 0 ? nums.size() - 1 : 0;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
