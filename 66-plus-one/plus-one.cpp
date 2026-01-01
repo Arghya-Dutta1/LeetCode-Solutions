c8ffd07eb77866fc4a392b3a98cd48e1547ee041
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-01-01 11:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (92.86%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& dig) {
+        vector<int> ans;
+        int n=dig.size()-1;
+        if(dig[n]<9){
+            ++dig[n];
+            return dig;
+        }
+        else{
+            while(n>=0 && dig[n]==9){
+                ans.push_back(0);
+                n--;
+            }
+            if(n>=0){
+                ans.push_back(++dig[n]);
+                for(int i=n-1;i>=0;i--)
+                    ans.push_back(dig[i]);
+            }
+            else
+                ans.push_back(1);
+            reverse(ans.begin(), ans.end());
+            return ans;
+        }
+    }
+};
