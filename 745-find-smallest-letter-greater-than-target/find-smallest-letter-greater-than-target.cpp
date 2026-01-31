3301635cfa12abeba5b6f2010f0ce52fb217a10c
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-01-31 19:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (88.80%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -4,13 +4,19 @@ public:
         int mn=26, idx=-1, it=-1;
         for(auto i:letters){
             it++;
-            if(i>target){
+            if(i>target)
                 if(static_cast<int>(i-target)<mn){
                     mn=i-target;
                     idx=it;
                 }
-            }
         }
         return idx==-1?letters[0]:letters[idx];
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
