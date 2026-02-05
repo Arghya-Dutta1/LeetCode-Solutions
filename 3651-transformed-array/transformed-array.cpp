cd88af0896bd2c1f5398548294b9882da1e070fc
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-02-05 17:43:16 +0530

    Time: 3 ms (89.69%) | Memory: 26 MB (8.08%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -4,9 +4,16 @@ public:
         int n = nums.size();
         vector<int> ans;
 
-        for (int i = 0; i <= n-1; ++i)
+        for (int i = 0; i < n; ++i)
             ans.push_back(nums[(i + nums[i] % n + n) % n]);
 
         return ans;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
