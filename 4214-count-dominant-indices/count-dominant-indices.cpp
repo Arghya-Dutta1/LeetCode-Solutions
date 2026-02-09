b001847523d15efa497cae060b41b3aa49f5682a
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-02-09 18:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 33.1 MB (0.00%) - LeetSync

---
+++ 4214-count-dominant-indices/count-dominant-indices.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dominantIndices(vector<int>& nums) {
+        int n=nums.size(), ans=0;
+        vector<int> ps(n, 0);
+        ps[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--)
+            ps[i]=ps[i+1]+nums[i];
+        for(int i=0;i<n-1;i++){
+            double avg=(double)ps[i+1] / (n - i - 1);
+            if(nums[i]>avg)
+                ans++;
+        }
+        return ans;
+    }
+};
