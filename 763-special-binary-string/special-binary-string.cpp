e18bfadab28fa2c9c80d1bb1db9a3bd37b89dff3
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-02-20 21:38:27 +0530

    Time: 1 ms (41.26%) | Memory: 9.5 MB (24.07%) - LeetSync

--- 763-special-binary-string/special-binary-string.cpp
+++ 763-special-binary-string/special-binary-string.cpp
@@ -23,3 +23,10 @@ public:
         return join(specials);
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
