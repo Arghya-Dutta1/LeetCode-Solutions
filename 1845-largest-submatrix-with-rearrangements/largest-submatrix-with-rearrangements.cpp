f450e76bb732c867ddf60dba56f3cbafea17977c
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-03-17 19:30:34 +0530

    Time: 32 ms (67.02%) | Memory: 90.1 MB (48.94%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -7,10 +7,8 @@ public:
         for (const vector<int>& row : matrix) {
             for (int i = 0; i < n; ++i)
                 hist[i] = row[i] == 0 ? 0 : hist[i] + 1;
-
             vector<int> sortedHist(hist);
             ranges::sort(sortedHist);
-
             for (int i = 0; i < n; ++i)
                 ans = max(ans, sortedHist[i] * (n - i));
         }
@@ -18,3 +16,10 @@ public:
         return ans;
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
