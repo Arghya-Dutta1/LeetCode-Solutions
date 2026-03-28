3c9f0b48a5a3c060b2278ad973769263b434b6b8
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-03-28 09:43:36 +0530

    Time: 7 ms (81.93%) | Memory: 75.4 MB (85.54%) - LeetSync

---
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+        char nonLetter = 'a' - 1, c = nonLetter;
+        vector<char> word(n, nonLetter);
+
+        for (int i = 0; i < n; ++i) {
+            if (word[i] != nonLetter)
+                continue;
+            if (++c > 'z')
+                return "";
+            for (int j = i; j < n; ++j)
+                if (lcp[i][j] > 0)
+                    word[j] = c;
+        }
+
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j) {
+                int nextLcp = i + 1 < n && j + 1 < n ? lcp[i + 1][j + 1] : 0;
+                int currLcp = word[i] == word[j] ? 1 + nextLcp : 0;
+                if (lcp[i][j] != currLcp)
+                    return "";
+            }
+
+        string ans;
+        for (auto c : word)
+            ans += c;
+        return ans;
+    }
+};
