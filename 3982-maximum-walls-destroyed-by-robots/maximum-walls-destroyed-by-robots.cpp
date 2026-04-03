262b8f7edaa9c10384d8ee0e37fe80f26f17d1ae
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-04-03 17:45:36 +0530

    Time: 332 ms (20.00%) | Memory: 229.4 MB (21.33%) - LeetSync

--- 3982-maximum-walls-destroyed-by-robots/maximum-walls-destroyed-by-robots.cpp
+++ 3982-maximum-walls-destroyed-by-robots/maximum-walls-destroyed-by-robots.cpp
@@ -45,3 +45,10 @@ public:
         return dfs(n - 1, 1);
     }
 };
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
