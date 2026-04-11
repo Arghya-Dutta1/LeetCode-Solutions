d4a07671e34ff8711cc7f28cababf11476f2aa89
Arghya Dutta <113010909+Arghya-Dutta1@users.noreply.github.com>
2026-04-11 11:43:54 +0530

    Time: 380 ms (61.00%) | Memory: 343.4 MB (65.00%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> g;
+        for (int i = 0; i < n; ++i) {
+            g[nums[i]].push_back(i);
+        }
+        int inf = 1 << 30, ans = inf;
+        for (auto& [_, ls] : g) {
+            int m = ls.size();
+            for (int h = 0; h < m - 2; ++h) {
+                int i = ls[h], k = ls[h + 2], t = (k - i) * 2;
+                ans = min(ans, t);
+            }
+        }
+        return ans == inf ? -1 : ans;
+
+    }
+};
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
